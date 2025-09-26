28ca93c4e6ff93b980b1192573411d877e08c7a9
XxcrygirlxX <elsuasnavassi@uide.edu.ec>
2025-09-25 21:47:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from typing import Union
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class login(BaseModel):
+    username: str
+    pwd: str
 
-    
+user_database = {
+    "username":"xcrygirlx",
+    "pwd":"hotdog1234"
+}
 
+@app.post("/login")
+def login(user: login):
+    if user.username == user_database["username"]:
+        if user.pwd == user_database["pwd"]:
+            return {"message": "login successful 👌"}
+        else:
+            return {"message": "incorrect password, try again :("}
+    else:
+        return {"message": "username not found"}
